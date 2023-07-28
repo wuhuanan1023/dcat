2347b12ef111f05236fe4ad8f57130bc6e284b8d
wuhuanan1023 <wuhuanan1023@163.com>
2023-07-28 13:34:04 +0800

    提交

--- app/Models/Devops/Platform/Platform.php
+++ app/Models/Devops/Platform/Platform.php
@@ -2,20 +2,20 @@
 
 namespace App\Models\Devops\Platform;
 
-use App\Models\Devops\BaseModel;
+use Dcat\Admin\Traits\HasDateTimeFormatter;
+
+use Illuminate\Database\Eloquent\Model;
+
 
 /**
  * 平台 - 如：大娱号、百度、京东、...
- * Class Platform
- * @package App\Models\Devops\Platform
  */
-class Platform extends BaseModel
+class Platform extends Model
 {
-
-
+	use HasDateTimeFormatter;
     protected $table = 'platform';
-    protected $guarded = [];
 
+    protected $guarded = [];
 
     #状态：0-禁用;1-启用;
     const STATUS_ON  = 1;
@@ -24,4 +24,5 @@ class Platform extends BaseModel
         self::STATUS_ON  => '启用',
         self::STATUS_OFF => '禁用',
     ];
+
 }
